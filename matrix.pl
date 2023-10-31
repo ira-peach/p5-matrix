03a5c7cbc16cd04590543da39fd8435e498a66a2
Ira Peach <yoshi_says_acab@proton.me>
2023-10-31 16:45:48 -0500

    Add usage to matrix.pl.

--- matrix.pl
+++ matrix.pl
@@ -25,15 +25,52 @@ use DateTime;
 use URI::Escape;
 use Getopt::Long qw(:config no_ignore_case no_auto_abbrev);
 use Term::Clui;
+use DDP;
+use File::Basename;
 
 use Net::Matrix qw(pp);
+
+sub usage_and_exit {
+    my ($status) = @_;
+
+    my $stream = *STDOUT;
+    $stream = *STDERR if $status;
+
+    my $program = basename $0;
+    my $usage =<<EOF;
+$program [OPTIONS]... -m MESSAGE
+$program -h
+
+Mandatory arguments to long options are mandatory for short options too.
+-h, --help
+    Display this help
+-u, --user NAME
+    User to log into home server as.  You may also set the environment variable MATRIX_USER instead.
+-p, --password PASSWORD
+    User password to log in with.  It is recommended to set the environment variable MATRIX_PASSWORD instead or omit this option to allow interactive input
+-r, --room-name NAME
+    Room name to find.  If multiple room names are returned, this will error.
+-H, --home-server SERVER
+    Home server to use.  Defaults to matrix.org.  You may also set the environment variable MATRIX_SERVER instead.
+-m, --message MESSAGE
+    Message to send; required.
+EOF
+
+    say $stream $usage;
+    say STDERR "ERROR: invalid invocation" if $status;
+
+    exit $status;
+}
 
 my $home_server;
 my $user;
 my $password;
 my $room_name;
 my $message;
+my $help;
+my $get_login_types;
 GetOptions(
+    "help|h" => \$help,
     "user|u=s" => \$user,
     "password|P=s" => \$password,
     "room-name|r=s" => \$room_name,
@@ -41,7 +78,11 @@ GetOptions(
     "message|m=s" => \$message)
 or die("error parsing command line arguments");
 
+usage_and_exit 0 if $help;
+usage_and_exit 1 unless $message;
+
 $home_server = $home_server // $ENV{MATRIX_SERVER} // ask("Home server: ", "matrix.org");
+
 $room_name = $room_name // ask("Room name: ", "Automation");
 $user = $user // $ENV{MATRIX_USER} // ask("User: ");
 $password = $password // $ENV{MATRIX_PASSWORD} // ask_password("Password ");
